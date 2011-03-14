3172e0e2b73ba8b151151e6acb638e195bb65b38
Sebastian Wojtowicz <qoo-ooq@gmx.de>
2011-03-14 02:27:41 +0100

    more cleanup in utf8convert

--- src/base/utf8convert.cpp
+++ src/base/utf8convert.cpp
@@ -1,11 +1,3 @@
-//
-//  utf8convert.c
-//  DDRaceAllisone
-//
-//  Created by Sebastian Wojtowicz on 13.03.11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
-
 #include "utf8convert.h"
 #include "system.h"
 
@@ -23,7 +15,8 @@ int is_utf8(const char * string)
 				 bytes[0] == 0x0D ||
 				 (0x20 <= bytes[0] && bytes[0] <= 0x7E)
 				 )
-		   ) {
+		   ) 
+		{
 			bytes += 1;
 			continue;
 		}
@@ -32,7 +25,8 @@ int is_utf8(const char * string)
 				 (0xC2 <= bytes[0] && bytes[0] <= 0xDF) &&
 				 (0x80 <= bytes[1] && bytes[1] <= 0xBF)
 				 )
-		   ) {
+		   ) 
+		{
 			bytes += 2;
 			continue;
 		}
@@ -54,7 +48,8 @@ int is_utf8(const char * string)
 			(0x80 <= bytes[1] && bytes[1] <= 0x9F) &&
 			(0x80 <= bytes[2] && bytes[2] <= 0xBF)
 			)
-		   ) {
+		   ) 
+		{
 			bytes += 3;
 			continue;
 		}
@@ -77,7 +72,8 @@ int is_utf8(const char * string)
 			(0x80 <= bytes[2] && bytes[2] <= 0xBF) &&
 			(0x80 <= bytes[3] && bytes[3] <= 0xBF)
 			)
-		   ) {
+		   ) 
+		{
 			bytes += 4;
 			continue;
 		}
@@ -87,7 +83,7 @@ int is_utf8(const char * string)
 	
 	return 1;
 }
-	
+
 std::string UTF8toLatin1( const char* szStr )
 {
 	
@@ -105,81 +101,111 @@ std::string UTF8toLatin1( const char* szStr )
 	
 	c = *(pSource);
 	
-	while (c) {
-		switch (state) {
+	while (c) 
+	{
+		switch (state) 
+		{
 			case 0:	/* start of utf8 char */
 				ucs4 = 0;	/* reset ucs4 char */
-				if ((c & 0xfe) == 0xfc) {		/* 6 octets */
+				if ((c & 0xfe) == 0xfc) 
+				{		/* 6 octets */
 					ucs4 = (c & 0x01) << 30;
 					octets = 6;
 					state = 5;	/* look for 5 more */
-				} else if ((c & 0xfc) == 0xf8) {	/* 5 octets */
+				} 
+				else if ((c & 0xfc) == 0xf8) 
+				{	/* 5 octets */
 					ucs4 = (c & 0x03) << 24;
 					octets = 5;
 					state = 4;
-				} else if ((c & 0xf8) == 0xf0) {	/* 4 octets */
+				} 
+				else if ((c & 0xf8) == 0xf0) 
+				{	/* 4 octets */
 					ucs4 = (c & 0x07) << 18;
 					octets = 4;
 					state = 3;
-				} else if ((c & 0xf0) == 0xe0) {	/* 3 octets */
+				} 
+				else if ((c & 0xf0) == 0xe0) 
+				{	/* 3 octets */
 					ucs4 = (c & 0x0f) << 12;
 					octets = 3;
 					state = 2;
-				} else if ((c & 0xe0) == 0xc0) {	/* 2 octets */
+				} 
+				else if ((c & 0xe0) == 0xc0) 
+				{	/* 2 octets */
 					ucs4 = (c & 0x1f) << 6;
 					octets = 2;
 					state = 1;	/* look for 1 more */
-				} else if ((c & 0x80) == 0x00) {	/* 1 octet */
+				} 
+				else if ((c & 0x80) == 0x00) 
+				{	/* 1 octet */
 					ucs4 = (c & 0x7f);
 					octets = 1;
 					state = 0;	/* we have a result */
-				} else {				/* error */
+				} 
+				else {				/* error */
 					;
 				}
 				break;
 			case 1:
-				if ((c & 0xc0) == 0x80) {
+				if ((c & 0xc0) == 0x80) 
+				{
 					ucs4 = ucs4 | (c & 0x3f);
-					if (ucs4 < 0x80 || ucs4 > 0x7ff) {
+					if (ucs4 < 0x80 || ucs4 > 0x7ff) 
+					{
 						ucs4 = 0xffffffff;
 					}
-				} else {
+				} 
+				else 
+				{
 					ucs4 = 0xffffffff;
 				}
 				state = 0;	/* we're done and have a result */
 				break;
 			case 2:
-				if ((c & 0xc0) == 0x80) {
+				if ((c & 0xc0) == 0x80) 
+				{
 					ucs4 = ucs4 | ((c & 0x3f) << 6);
 					state = 1;
-				} else {
+				} 
+				else 
+				{
 					ucs4 = 0xffffffff;
 					state = 0;
 				}
 				break;
 			case 3:
-				if ((c & 0xc0) == 0x80) {
+				if ((c & 0xc0) == 0x80) 
+				{
 					ucs4 = ucs4 | ((c & 0x3f) << 12);
 					state = 2;
-				} else {
+				} 				
+				else 
+				{
 					ucs4 = 0xffffffff;
 					state = 0;
 				}
 				break;
 			case 4:
-				if ((c & 0xc0) == 0x80) {
+				if ((c & 0xc0) == 0x80) 
+				{
 					ucs4 = ucs4 | ((c & 0x3f) << 18);
 					state = 3;
-				} else {
+				} 
+				else 
+				{
 					ucs4 = 0xffffffff;
 					state = 0;
 				}
 				break;
 			case 5:
-				if ((c & 0xc0) == 0x80) {
+				if ((c & 0xc0) == 0x80) 
+				{
 					ucs4 = ucs4 | ((c & 0x3f) << 24);
 					state = 4;
-				} else {
+				} 
+				else 
+				{
 					ucs4 = 0xffffffff;
 					state = 0;
 				}
@@ -189,8 +215,10 @@ std::string UTF8toLatin1( const char* szStr )
 				state = 0;
 				break;
 		}
-		if (state == 0) {
-			switch (octets) {
+		if (state == 0) 
+		{
+			switch (octets) 
+			{
 				case 1:
 					if (ucs4 < 0x0 || ucs4 > 0x7f)
 						ucs4 = 0xffffffff;
@@ -219,7 +247,8 @@ std::string UTF8toLatin1( const char* szStr )
 					ucs4 = 0xffffffff;
 					break;
 			}
-			if (ucs4 != 0xffffffff) {
+			if (ucs4 != 0xffffffff) 
+			{
 				strResult += (char)ucs4;
 			}
 		}
@@ -228,7 +257,6 @@ std::string UTF8toLatin1( const char* szStr )
 	return strResult;
 }
 	
-// By Ben Bryant	
 std::string Latin1toUTF8( const char* szStr )
 {
 	const unsigned char* pSource = (const unsigned char*)szStr;
